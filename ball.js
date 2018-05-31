1a8ca5a8eb1af1fd270e4f58f9393603c0a5e508
CarolBrot <39806427+CarolBrot@users.noreply.github.com>
2018-05-31 12:20:10 -0500

    uploading files

---
+++ ball.js
@@ -0,0 +1,8 @@
+var length = data.length;
+
+// you work goes here
+// -----------------------
+var length=data.length;
+for(var i=0; i<length;i++)
+{var ball=data[i];
+create(ball.x,ball.y,ball.color);}
